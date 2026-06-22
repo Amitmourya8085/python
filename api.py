c11de280b48782eb6715cdf32fbb2e07e819911a
Amit Lalji mourya <amitmourya8085@gmail.com>
2026-06-22 17:45:52 +0530

    updated api basic file

--- api.py
+++ api.py
@@ -1,4 +1,11 @@
 import requests
+
 url = "https://api.github.com/users/octocat"
+
 response = requests.get(url)
-print(response.status_code)
+
+# print(response.status_code)
+# print(response.text)
+
+data = response.json()
+print(data["login"])
