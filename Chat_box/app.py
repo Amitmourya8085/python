29b8432b1b7e760aa9be67dc2b65b294db5863e7
Amit Lalji mourya <amitmourya8085@gmail.com>
2026-06-28 15:58:22 +0530

    resolve the api key problem and post also

--- Chat_box/app.py
+++ Chat_box/app.py
@@ -1,7 +1,7 @@
 import requests
 from flask import Flask,request,render_template,jsonify
 
-API_KEY = "AQ.Ab8RN6IUSukRqfIf_gv2FuO3yaLuj6zT8XUnS36OOEN9c215Lg"
+API_KEY = "AQ.Ab8RN6IyeWQsXf0KnPHoK9yyrcNAT4IgAX9l7FXyT1m5GclGtw"
 URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={API_KEY}"
 
 app = Flask(__name__)
@@ -10,7 +10,7 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/Ask')
+@app.route('/ask',methods=['POST'])
 def ask_AM8085():
      user_prompt = request.json.get('prompt')
      payload = {
@@ -21,7 +21,16 @@ def ask_AM8085():
      
      header = {"Contents-Type":"applicatin/type"}
 
-     
+     response = requests.post(url=URL,json=payload,headers=header)
+     data = response.json()
+
+     answer = data['candidates'][0]['content']['parts'][0]['text']
+
+     return jsonify({"response": answer})
+
+
+if __name__=='__main__':
+     app.run(debug=True)
 
 
 
