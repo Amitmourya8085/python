7752ceb248aa89b3cbc9def7b045983874365f38
Amitmourya8085 <amitmourya8085@gmail.com>
2026-06-28 03:15:18 -0600

    Fix typo: rename wed_app to web_app

--- weather.py
+++ weather.py
@@ -1,4 +1,5 @@
 import requests
+import tkinter as tk
 
 api_key = "dfba527088c26160a04332e6f7ab004f"
 city = input("Enter city:")
@@ -22,3 +23,23 @@ if response.status_code == 200:
 else:
     print(f"HTTPS Error occurse ({response.status_code})")
     print(response.text)
+
+# UI setup
+root = tk.Tk()
+root.title("Weather App")
+
+root.geometry("300x300")
+
+title = tk.Label(root, text="Weather App 🌦️", font=("Arial", 16))
+title.pack(pady=10)
+
+city_entry = tk.Entry(root, width=25)
+city_entry.pack(pady=10)
+
+search_btn = tk.Button(root, text="Get Weather", command=get_weather)
+search_btn.pack(pady=10)
+
+result_label = tk.Label(root, text="", font=("Arial", 12))
+result_label.pack(pady=20)
+
+root.mainloop()    
