006789fabf981ef4df33afd3f0f49a794bac267e
Amit Lalji mourya <amitmourya8085@gmail.com>
2026-07-02 13:59:51 +0530

    done some api security and added exception handling

--- Chat_box/app.py
+++ Chat_box/app.py
@@ -1,13 +1,16 @@
 import requests
 from flask import Flask, request, render_template, jsonify, session
+from dotenv import load_dotenv
+import os
 
-API_KEY = "AQ."
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
 URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={API_KEY}"
 
 app = Flask(__name__)
 
-
-app.secret_key = "amit_mourya_super_secret_key"
+app.secret_key = os.getenv('secret_key') or 'fallback_secret_key'
 
 @app.route('/')
 def home():
@@ -18,7 +21,8 @@ def home():
 @app.route('/ask', methods=['POST'])
 def ask_AM8085():
     user_prompt = request.json.get('prompt')
-    
+    if not user_prompt:
+        return jsonify({'response": "Please enter something!'})
     
     chat_history = session.get('history', [])
     
@@ -34,17 +38,22 @@ def ask_AM8085():
     } 
      
     header = {"Content-Type": "application/json"}
+    
+    try:
+        response = requests.post(url=URL, json=payload, headers=header)
+        data = response.json()
 
-    response = requests.post(url=URL, json=payload, headers=header)
-    data = response.json()
+       
+        answer = data['candidates'][0]['content']['parts'][0]['text']
 
-    answer = data['candidates'][0]['content']['parts'][0]['text']
+    except Exception as e:
+        print("Error:", e)
+        answer = "⚠️ Something went wrong. Please try again."
 
     chat_history.append({
         "role": "model",
         "parts": [{"text": answer}]
     })
-
 
     session['history'] = chat_history
 
